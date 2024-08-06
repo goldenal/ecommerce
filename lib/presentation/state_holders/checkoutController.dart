a616cbda5195162854d185866a012b083cd1c76b
adeosun adewale <goldenal86@gmail.com>
2024-08-06 17:26:04 +0100

    "Updated pay method in CheckoutController to use test secret key and reformatted code"

--- lib/presentation/state_holders/checkoutController.dart
+++ lib/presentation/state_holders/checkoutController.dart
@@ -5,27 +5,31 @@ import 'package:get/get.dart';
 import 'package:pay_with_paystack/pay_with_paystack.dart';
 
 class Checkoutcontroller extends GetxController {
-
-
-  pay()async{
+  pay() async {
     final uniqueTransRef = PayWithPayStack().generateUuidV4();
- 
- PayWithPayStack().now(
-    context: Get.context as BuildContext,
-    secretKey:"sk_live_XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    customerEmail: "your@email.com",
-    reference:uniqueTransRef, 
-    callbackUrl: "thefluxway.com",
-    currency: "NGN",
-    paymentChannel:["card", "bank", "ussd", "qr", "mobile_money", "bank_transfer", "eft"],
-    amount: 200,
-    transactionCompleted: () {
-        log("Transaction Successful");
-    },
-    transactionNotCompleted: () {
-        log("Transaction Not Successful!");
-    });
+
+    PayWithPayStack().now(
+        context: Get.context as BuildContext,
+        secretKey: "sk_test_3841acc7b3c03f7403e1fd92f534c05cbafabc4f",
+        customerEmail: "your@email.com",
+        reference: uniqueTransRef,
+        callbackUrl: "thefluxway.com",
+        currency: "NGN",
+        paymentChannel: [
+          "card",
+          "bank",
+          "ussd",
+          "qr",
+          "mobile_money",
+          "bank_transfer",
+          "eft"
+        ],
+        amount: 200,
+        transactionCompleted: () {
+          log("Transaction Successful");
+        },
+        transactionNotCompleted: () {
+          log("Transaction Not Successful!");
+        });
   }
-
 }
-
